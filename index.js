83dc9dcafb0a8a8cc877f4dae1736eb982d9ba9b
neclude <neclude.wildforest@gmail.com>
2016-12-13 10:49:26 +0700

    Просовываем наружу getImageFormat

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var gd = module.exports = Object.create(require('node-gd')),
     _ = require('underscore')
 
 _.extend(gd,
-    require('./lib/errors.js').classes,
-    require('./lib/opener.js'),
-    require('./lib/transform.js')
+         require('./lib/errors.js').classes,
+         require('./lib/opener.js'),
+         require('./lib/transform.js'),
+         require('./lib/formats.js').getImageFormat
 )
 
 _.extend(gd.Image.prototype,
-    require('./lib/image.js')
+         require('./lib/image.js')
 )
